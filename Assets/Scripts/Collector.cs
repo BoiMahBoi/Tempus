a1d9e78bbec8e2ad7a30fe049dc3f349a43b1f7e
Tjums <thomasfb@live.dk>
2023-03-24 18:01:43 +0100

    Collector 2.0

--- Assets/Scripts/Collector.cs
+++ Assets/Scripts/Collector.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class Collector : MonoBehaviour
 {
-    //Script is a player component
-    //Script requires collectables to hold the tag "Collectable"
+    private collectableTracker collectable;
+	
 
+	private void Start()
+	{
+		collectable = GameObject.Find("ExitTrigger").GetComponent<collectableTracker>();
+	}
     //Method for when "Player" Collider2D collides with other Collider2D's
     private void OnTriggerEnter2D(Collider2D collider2D)
     {
         //On collision with gameobjects tagged "Collectable"
         if (collider2D.gameObject.CompareTag("Collectable"))
         {
+			//Calls function collectedIncrementer of collectableTracker script
+			collectable.collectedIncrementer();
             //Destroy collided gameobject with tag "Collectable"
             Destroy(collider2D.gameObject);
         }
--- Assets/Scripts/collectableTracker.cs
+++ Assets/Scripts/collectableTracker.cs
@@ -1,32 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class collectableTracker : MonoBehaviour
 {
+    //String for the tag to detect
     [SerializeField] private string tagToDetect = "Collectable";
+    //Array of all the collectables in the scene
     [SerializeField] private GameObject[] allCollectables;
-    [SerializeField] private bool allCollected;
+    //Integer holding the value of the number of collected collectables
     [SerializeField] public int numberOfCollected;
 
     void Start()
     {
-        //Finding all gameobjects with the tag "Collectable" and adding them to the Array allCollectables
+        //Finding all gameobjects with the specified tag and adding them to the Array allCollectables
         allCollectables = GameObject.FindGameObjectsWithTag(tagToDetect);
     }
 
-    void Update()
+    //Function called from Collector script, when collectable have been collected
+    public void collectedIncrementer()
     {
-        
+        //Increments numberOfCollected by 1
+        numberOfCollected++;
     }
     
     private void OnTriggerEnter2D(Collider2D collider2D)
     {
-        //On collision with gameobjects tagged "Player" and the number of collected is equal the amount.
+        //On collision with gameobject tagged "Player" and all collectables have been collected.
         if (collider2D.gameObject.CompareTag("Player") && numberOfCollected == allCollectables.Length)
         {
-            //Destroy collided gameobject with tag "Player"
-            Destroy(collider2D.gameObject);
+            //Loading next scene in the Build Index
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
     }
 }
